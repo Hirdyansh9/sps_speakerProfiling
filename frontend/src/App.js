28ce4ad72c465d86760bce3007acf2c9d0db7b0e
Arnav Gupta <147974674+arnav0418@users.noreply.github.com>
2025-05-15 19:57:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
+//App.js
+
 import React, { useState } from 'react';
 import Record from './Record';
 import Result from './Result';
+import './App.css';
 
 function App() {
   const [result, setResult] = useState(null);
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div className="container">
       <h1>Speaker Attribute Predictor</h1>
       <Record setResult={setResult} />
       {result && <Result result={result} />}
